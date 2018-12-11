2f06d21eabf1f037b1a7866682f5cb8189d8e466
verixx <abdurraqeeb53@gmail.com>
2018-12-11 15:47:14 +1100

    Use deque instead for popping

--- solutions/08.py
+++ solutions/08.py
@@ -1,5 +1,6 @@
 from aocd import data
 from dataclasses import dataclass, field
+from collections import deque
 from utils import timed
 
 @dataclass
@@ -8,17 +9,17 @@ class Node:
     metadata: list = None
 
     @classmethod
-    def from_data(cls, data):
+    def from_data(cls, data: deque):
         node = cls()
 
-        num_children = data.pop(0)
-        num_metadata = data.pop(0)
+        num_children = data.popleft()
+        num_metadata = data.popleft()
 
-        for x in range(num_children):
+        for _ in range(num_children):
             child = cls.from_data(data)
             node.children.append(child)
 
-        node.metadata = [data.pop(0) for x in range(num_metadata)]
+        node.metadata = [data.popleft() for _ in range(num_metadata)]
         return node
     
     @property
@@ -36,7 +37,7 @@ class Node:
         return sum(self.metadata) + sum(c.metasum for c in self.children)
 
 
-input = [int(i) for i in data.split()]
+input = deque(int(i) for i in data.split())
 root = Node.from_data(input)
 
 print(root.metasum)
